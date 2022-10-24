d1f6db6fb6f13cca5f1e3cf27267a2456a44187f
mahmoud sabri <mahmoud.sabri.abdelnabi@gmail.com>
2022-10-24 14:29:20 +0200

    add FullScreen Feature

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-// import {Settings} from './Settings.js'
-
-// let settings = new Settings();
-
-
 import { Settings } from './Settings.js'
 
 let settings = new Settings();
 
+
+var bodyDom = document.querySelector("body");
+
+
+
+
+
+$("#fullBtn ,#exitFullBtn").click(function () {
+    console.log("545")
+    if (document.fullscreenElement) {
+        // $(".fullScreenBtn").html(`<i class="fa-solid fa-up-right-and-down-left-from-center"></i>`)
+        $(".fa-minimize").fadeOut(400, function () {
+            $(".fa-up-right-and-down-left-from-center").fadeIn(200)
+        })
+        document.exitFullscreen()
+    } else {
+        // $(".fullScreenBtn").html(`<i class="fa-solid fa-minimize"></i>`)
+        $(".fa-up-right-and-down-left-from-center").fadeOut(400, function () {
+            $(".fa-minimize").fadeIn(200)
+        })
+        bodyDom.requestFullscreen()
+    }
+})
 
